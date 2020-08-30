5e0599387487d8e6f0b4900206d95b9efc842e29
Fábio Santos <fabio.santos@fundacaoabc.org>
2020-08-30 15:22:44 -0300

    Criado appointment repository para armazenar a lógica de criação de appointments, listar todos e encontrar appointment por date

---
+++ src/repository/AppointmentsRepository.ts
@@ -0,0 +1,25 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../model/Appointment';
+
+class AppointmentsRepository {
+  private appointments = Array<Appointment>();
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+    this.appointments.push(appointment);
+    return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+    return findAppointment || null;
+  }
+
+  public getAllAppointments(): Appointment[] {
+    return this.appointments;
+  }
+}
+
+export default AppointmentsRepository;
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,33 +1,28 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repository/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
-const appointments = Array<Appointment>();
-// const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (req, res) => res.json(appointments));
+appointmentsRouter.get('/', (req, res) =>
+  res.json(appointmentsRepository.getAllAppointments()),
+);
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentInSameDate) {
     return res.status(400).json({ message: 'This hour is already used' });
   }
 
-  const appointment = { id: uuid(), provider, date: parsedDate };
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return res.json(appointment);
 });
