3e7523025ba33234e1523fcc9d435083df28a53b
Fábio Santos <fabio.santos@fundacaoabc.org>
2020-09-06 16:58:25 -0300

    feat: acesso aos arquivos de avatar
    Criada uma rota /files que permite acessar os arquivos de avatar armazenados em /tmp

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 import './database';
+import upload from './config/upload';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use('/files', express.static(upload.directory));
 
 app.listen(7000, () => console.log('Server running on port 7000'));
