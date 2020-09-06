2f9d2fad0d3be136ff6fef180b5c52a4c7376137
Fábio Santos <fabio.santos@fundacaoabc.org>
2020-09-06 12:00:03 -0300

    feat: alteração do campo provider para provider_id em appointments
    Alteração do campo provider para provider_id em appointments realizando o relacionamento entre
    appointments e user, tanto nas tabelas quanto no model

---
+++ src/database/migrations/1599403015488-AppointmentProviderToProviderId.ts
@@ -0,0 +1,48 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AppointmentProviderToProviderId1599403015488
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('appointments', 'provider');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'Appointments_provider_id_FK',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'appointments',
+      'Appointments_provider_id_FK',
+    );
+    await queryRunner.dropColumn('appointments', 'provider_id');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider',
+        type: 'varchar(100)',
+        isNullable: false,
+      }),
+    );
+  }
+}
