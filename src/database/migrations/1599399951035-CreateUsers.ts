a5f114e8e62af0a6a7d7345e63820db520d7a280
Fábio Santos <fabio.santos@fundacaoabc.org>
2020-09-06 11:06:54 -0300

    feat: criação do model e migration de users e alterações em appointments
    Alterado estlint para ignorar camelcase, crida migration e model para users, adicionadas colunas
    created_at e updated_at na migration de appointments

--- src/database/migrations/1599399951035-CreateUsers.ts
+++ src/database/migrations/1599399951035-CreateUsers.ts
@@ -1,27 +1,30 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1599332638574
-  implements MigrationInterface {
+export default class CreateUsers1599399951035 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
             type: 'uuid',
             isPrimary: true,
-            generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar(100)',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar(50)',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar(50)',
             isNullable: false,
           },
           {
@@ -42,6 +45,6 @@ export default class CreateAppointments1599332638574
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
