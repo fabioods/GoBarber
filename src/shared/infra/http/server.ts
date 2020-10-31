b30722ec1df2b4ea550bf7e593444b7f4ef8d3bd
Fábio Santos <fabio.santos@fundacaoabc.org>
2020-10-31 10:23:53 -0300

    chore: ajustes finais que faltaram na refatoração, e testes

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -12,7 +12,7 @@ import '@shared/container';
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(upload.directory));
+app.use('/files', express.static(upload.uploadFolder));
 app.use(routes);
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
