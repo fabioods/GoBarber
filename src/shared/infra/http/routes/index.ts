af48b1d379052955b5c19a9a18cebaf2ce2d60e7
Fábio Santos <fabio.santos@fundacaoabc.org>
2020-11-02 09:46:47 -0300

    feat: criação de rotas e serviços

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import appointmetsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import userRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 routes.use('/appointments', appointmetsRouter);
 routes.use('/users', userRouter);
 routes.use('/sessions', sessionRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
