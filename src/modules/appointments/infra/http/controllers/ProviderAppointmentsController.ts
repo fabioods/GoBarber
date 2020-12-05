9b3c430eef4fbd0672e4810370f0dd709083b6b1
Fábio Santos <fabio.santos@fundacaoabc.org>
2020-12-05 11:18:32 -0300

    feat: mudanças para request.body para request.query nos controllers de provider

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -6,15 +6,15 @@ import { classToClass } from 'class-transformer';
 export default class ProvidersAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
     const listProviderService = container.resolve(
       ListProviderAppointmentsService,
     );
     const listProviders = await listProviderService.execute({
       provider_id,
-      day,
-      month,
-      year,
+      month: Number(month),
+      year: Number(year),
+      day: Number(day),
     });
     return response.json(classToClass(listProviders));
   }
--- src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -5,15 +5,15 @@ import { container } from 'tsyringe';
 export default class ProviderMonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { month, year, day } = request.body;
+    const { month, year, day } = request.query;
     const listProviderDayAvailabilityService = container.resolve(
       ListProviderDayAvailabilityService,
     );
     const availability = await listProviderDayAvailabilityService.execute({
       provider_id,
-      month,
-      year,
-      day,
+      month: Number(month),
+      year: Number(year),
+      day: Number(day),
     });
     return response.json(availability);
   }
